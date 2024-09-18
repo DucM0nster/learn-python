9288cc887b12c097a50f8fc11af4be50e5a53f5d
xDucMonster <96818219+xDucMonster@users.noreply.github.com>
2024-09-18 22:26:31 +0200

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,14 @@
+
+numbers = [2, 4, 8, 16, 32, 64]
+animals = ["Dog", "Horse", "Crocodile", "Wolf", "Tiger"]
+#            0       1         2           3       4
+# animals = ["Dog", 2, False]
+
+print(animals)
+print(animals[1])
+print(animals[-1])
+print(animals[1:])
+print(animals[1:3])
+
+animals[1] = "Unicorn"
+print(animals[1])
---
+++ lists1.py
@@ -0,0 +1,27 @@
+
+numbers = [2, 4, 8, 16, 32, 64, 37]
+animals = ["Dog", "Horse", "Crocodile", "Wolf", "Wolf", "Tiger"]
+
+# animals.extend(numbers)
+# animals.append("Creeper")
+# animals.insert(1, "Zombie")
+# animals.remove("Dog")
+# animals.clear()
+# animals.pop()
+
+# print(animals)
+# print(animals.index("Dog"))
+# print(animals.index("Gepard"))
+# print(animals.count("Wolf"))
+
+# animals.sort()
+# print(animals)
+#
+# numbers.sort()
+# print(numbers)
+
+# numbers.reverse()
+# print(numbers)
+
+animals2 = animals.copy()
+print(animals2)
