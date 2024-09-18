98252e60ec746d182f621f1d3b3b617dc4e238ef
xDucMonster <96818219+xDucMonster@users.noreply.github.com>
2024-09-18 22:13:59 +0200

    Add files via upload

---
+++ text.py
@@ -0,0 +1,14 @@
+# Ausgabe ins Terminal
+print("hello")
+
+# Man kann einfach eine Variable festlegen, so kann man einfach den Namen ändern ohne alles im Code ändern zu müssen
+character_name = "Hans" #String
+character_age = 403.42 #Number
+is_male = True  #True or False
+print("Hallo, ich heiße " + character_name + ", ")
+print("außerdem bin ich " + character_age + " Jahre alt.")
+
+# Hier wird der Wert der Variable überschrieben
+character_name = "Rick"
+print(character_name + " is a scientist.")
+print("He travels trough time and space")
