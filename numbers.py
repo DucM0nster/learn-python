94c26635f891a13bed945619ebcf70ef0334ef21
xDucMonster <96818219+xDucMonster@users.noreply.github.com>
2024-09-18 22:12:54 +0200

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,49 @@
+from math import*
+#includes another/a lot more math functions
+#list on the internet with exactly functions
+
+print(42)
+print(3+4)
+print(3+4*5)
+print(3*(4+5))
+
+#Rest
+print(10 % 3)
+print(10 % 6)
+
+a_num = 5
+print(a_num)
+
+#into str
+print(str(a_num) + " is the half of 10")
+
+#failure
+print(a_num + " is the half of 10")
+print(str(a_num) + "5")
+print(str(a_num) + " 5")
+
+#absolute value
+aa_num = -5
+print(abs(aa_num))
+
+#wird mit letzterem quadrierd 256 2^8
+print(pow(2, 8))
+
+#choose the higher one
+print(max(7, 6))
+
+#lower
+print(min(7, 6))
+print(round(5.3))
+print(round(5.7))
+
+#math import things
+#rundet immer ab
+
+print(floor(5.6))
+
+#rundet immer auf
+print(ceil(5.6))
+
+#Wurzel ziehen
+print(sqrt(36))
