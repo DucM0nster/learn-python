fac5c56e163f56e0f87277b41903043c76952461
xDucMonster <96818219+xDucMonster@users.noreply.github.com>
2024-09-18 22:16:21 +0200

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,9 @@
+
+color1 = input("Farbe: ")
+adjektiv = input("Adjektiv: ")
+wo = input("Geschäft: ")
+print("Rosen sind " + color1)
+print("Deine Mutter ist " + adjektiv)
+print("und klaut bei " + wo)
+
+
